902f6c4a6d98e2e30bf5d9b676959ddacc4af1bb
elgraphica <elferenczi@outlook.com>
2023-02-20 09:47:35 +0000

    array of questsions complete

--- index.js
+++ index.js
@@ -2,52 +2,60 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-const questions = [
-//Title
-{
-    type: 'input',
-    name: 'title',
-    message: 'What is your project title?'
-},
-//Description
-{
-    type: 'input',
-    name: 'description',
-    message: 'Put your project description here'
-},
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What are the usage instructions for your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which license would you like to use for your project?',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines for your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ])
+  .then((response) => writeFileAsync('README.md', generateMarkdown(response)))
+  .then(() => console.log('README.md generated successfully!'))
+  .catch((err) => console.error(err));
+  
 
-//Licence
-{
-    type: 'list',
-    choices: ['MIT', 'GPL', 'GNU', 'Bsd-2-Clause'],
-    name: 'licence',
-    message: 'What is your project licence type?'
-}
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('README.md', data, (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('Success!');
-        }
-    });
-}
-
-// function to initialize program
-function init() {
-    inquirer
-    .prompt(questions)
-    .then(answear => {
-console.log(answear);
-const markdown = generateMarkdown(answear);
-    });
-
-}
-
-// function call to initialize program
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,5 @@
-// function to generate markdown for README
-function generateDescription(description) {
-  return `
-  ## Description
-  ${description}
-  `;
-}
+//function to generate markdown
 
-function generateMarkdown(data) {
-  return `# ${data.title}
-  ${data.description !== '' ? generateDescription(data.description) : ''}
-  
-  ## Licence
-  ${data.licence}
-`;
-}
+
 
 module.exports = generateMarkdown;
