fece48b0544cb10ad3094567428ce09d7a052b3c
elgraphica <elferenczi@outlook.com>
2023-02-20 10:15:19 +0000

    generateMarkdown complete(tested)

---
+++ testREADME.md
@@ -0,0 +1,41 @@
+
+# Generate Markdown
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+9 questsions will pop up that will generate your whole README.md file
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+Answear the questsions 
+
+## License
+
+This project is covered under the MIT license.
+
+## Contributing
+
+N/A
+
+## Tests
+
+N/A
+
+## Questions
+
+If you have any questions about the project, you can reach me through my [GitHub profile](https://github.com/elgraphica) or email me at elferenczi@xxxxxxx.com.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,48 @@
 //function to generate markdown
 
+function generateMarkdown(response) {
+  return `
+# ${response.title}
 
+${response.license === 'MIT' ? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' : ''}
+
+## Description
+
+${response.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${response.installation}
+
+## Usage
+
+${response.usage}
+
+## License
+
+This project is covered under the ${response.license} license.
+
+## Contributing
+
+${response.contributing}
+
+## Tests
+
+${response.tests}
+
+## Questions
+
+If you have any questions about the project, you can reach me through my [GitHub profile](https://github.com/${response.username}) or email me at ${response.email}.
+`;
+}
 
 module.exports = generateMarkdown;
